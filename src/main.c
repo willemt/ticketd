33886cba765115f588d2648dca5ed8d0b18cbb42
Rinat Ibragimov <ibragimovrinat@mail.ru>
2018-06-09 01:59:38 +0300

    Use constant expression for IP_STR_LEN
    GCC no longer treats strlen("111.111.111.111") as a constant, and complains
    about it being used for array size.

--- src/main.c
+++ src/main.c
@@ -34,7 +34,7 @@
 #define LEADER_URL_LEN 512
 #define IPC_PIPE_NAME "ticketd_ipc"
 #define HTTP_WORKERS 4
-#define IP_STR_LEN strlen("111.111.111.111")
+#define IP_STR_LEN (sizeof("111.111.111.111") - 1)
 
 typedef enum {
     HANDSHAKE_FAILURE,
